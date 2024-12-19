f0f191aad19ddec0471f85764c489c36a009abe7
Trina Hoang Ngo <trinahngo311@gmail.com>
2024-12-19 01:04:51 -0800

    passed all test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
-};
+const reverseString = function (string) {
+    let reversedString = "";
+    let newArray = Array.from(string);
+  
+    for (let i = 0; i < string.length; i++) {
+      let letter = newArray.pop();
+      reversedString += letter;
+    }
+  
+    return reversedString;
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
