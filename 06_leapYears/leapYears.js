253f2adabd5d607d5188661163101ef2658c3df2
Trina Hoang Ngo <trinahngo311@gmail.com>
2024-12-27 01:06:11 -0800

    corrected the loop for better understanding

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(year) {
-    if (year % 4 === 0 && year % 100) {
+    if (year % 4 === 0 && year % 100 != 0) {
         return true;
     } else if (year % 4 === 0 && year % 400 === 0) {
         return true;
