6aae73423e6d3dbea9268ab21709877dfaba4fef
Trina Hoang Ngo <trinahngo311@gmail.com>
2024-12-28 00:27:01 -0800

    passed all 6 cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegree) {
+  let celsiusFormula = (fahrenheitDegree - 32) * (5/9);
+  let fahrenheitToCelsius = Math.round(celsiusFormula * 10) / 10;
+  return fahrenheitToCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegree) {
+  let fahrenheitFormula = (celsiusDegree * (9/5)) + 32;
+  let celsiusToFahrenheit = Math.round(fahrenheitFormula * 10) / 10;
+  return celsiusToFahrenheit;
 };
 
 // Do not edit below this line
